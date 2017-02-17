19df10af8c0a44fd0d4fc0af453110e3e6f8dd09
Leonardo Saraiva <vyper@maneh.org>
2017-02-17 10:29:35 -0200

    Add queue worker

---
+++ lib/grpc/kit/queue/worker.rb
@@ -0,0 +1,18 @@
+require_relative 'worker/runner'
+
+module GRPC
+  module Kit
+    module Queue
+      module Worker
+        def self.included(klass)
+          @@workers ||= []
+          @@workers << klass
+        end
+
+        def call
+          raise NotImplemented.new
+        end
+      end
+    end
+  end
+end
